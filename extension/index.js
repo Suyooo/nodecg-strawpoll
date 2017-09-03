dc905fc09b9550e2eec05027bc6dfcbd78e743a8
Tyler Hardy <tylerthardy@gmail.com>
2017-09-03 05:46:03 -0500

    Update to 0.8.x

---
+++ extension/index.js
@@ -0,0 +1,20 @@
+module.exports = function(nodecg) {
+    var express = require('express'),
+        app = express(),
+        request = require('request');
+
+    app.get('/strawpoll/:id', function(req, res) {
+		url = 'http://strawpoll.me/api/v2/polls/'+req.params.id;
+
+		request(url, function(error, response, out){
+			if(!error){
+				res.writeHead(200, {
+					'Content-Type': 'text/plain',
+					'Expires': new Date().toUTCString()
+				});
+				res.end(out);
+			}
+		})
+    });
+    nodecg.mount(app);
+};
